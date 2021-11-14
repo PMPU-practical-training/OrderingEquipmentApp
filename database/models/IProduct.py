b4bc6435b0c6244d35f746f5f3665a851dfcf067
Grigorev Grigorii <nigddd@gmail.com>
2021-11-14 20:19:14 +0300

    Update IProduct.py

--- database/models/IProduct.py
+++ database/models/IProduct.py
@@ -14,6 +14,7 @@ class Product(Base):
     verified_by = Column(Text, default=None)
     requires_verification = Column(Boolean, default=False)
     price = Column(Float, nullable=True)
+    subcategory_id = Column(Integer, default=None)
 
     def __repr__(self):
         return "{}: {}".format(self.__name__, ', '.join(['{}={}'.format(key, value) for key, value in self.__dict__ if not key.startswith('__')]))
