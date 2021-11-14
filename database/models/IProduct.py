ace04604801dabb30e0583869393423635ce3f6d
Grigorev Grigorii <nigddd@gmail.com>
2021-11-14 20:16:06 +0300

    Update IProduct.py

--- database/models/IProduct.py
+++ database/models/IProduct.py
@@ -1,27 +1,19 @@
-from werkzeug.security import generate_password_hash,  check_password_hash
-
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, Text, Boolean
 from database.__main__ import Base
 
 
 class Product(Base):
     """
-    Пользователи
+    Продукты
     """
 
     __tablename__ = "product"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False, unique=True)
-    verifiedBy = Column(String, nullable=False)
-    requiresVerefication = Column(Boolean(), primary_key=True)
-    price = Column(Integer, nullable=True)
-
-
-
-
-    def get_id(self):
-        return self.Product_id
+    name = Column(Text, nullable=False, unique=True)
+    verified_by = Column(Text, nullable=False)
+    requires_verefication = Column(Boolean)
+    price = Column(Float, nullable=True)
 
     def __repr__(self):
         return "{}: {}".format(self.__name__, ', '.join(['{}={}'.format(key, value) for key, value in self.__dict__ if not key.startswith('__')]))
